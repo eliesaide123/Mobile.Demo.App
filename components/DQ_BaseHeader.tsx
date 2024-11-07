8dc662cbd39b76b7704a026d6355d22b4a62a0f0
elie-s <e.saide@dq.com.lb>
2024-11-07 16:01:43 +0200

    --added the Change Role to the App

--- components/DQ_BaseHeader.tsx
+++ components/DQ_BaseHeader.tsx
@@ -1,12 +1,14 @@
 import Icon from '@react-native-vector-icons/fontawesome6';
 import React from 'react';
 import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import DQ_Button from './DQ_Button';
 
 const logo = require('../assets/images/DQ_LOGO.png'); //
 
 export default function DQ_BaseHeader({navigation, press} : any) {
     return (
         <View style={styles.header}>
+            <DQ_Button title="Roles" onPress={() => navigation.navigate('Roles')}/>
             <TouchableOpacity style={styles.backButton} onPress={press}>
                 <Icon name="chevron-left" size={18} color="#FFFFFF" iconStyle='solid' />
             </TouchableOpacity>
---
+++ screens/product-policy-screen/product-policy.tsx
@@ -0,0 +1,233 @@
+import {useEffect, useState} from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ScrollView,
+} from 'react-native';
+import {ProductPolicyService} from './service/product-policy.service';
+import DQ_BaseHeader from '../../components/DQ_BaseHeader';
+import DQ_Card from '../../components/DQ_Card';
+import DQ_InnerCard_Grid from '../../components/DQ_InnerCard_Grid';
+import DQ_Paragraph from '../../components/DQ_Paragraph';
+import DQ_GoButton from '../../components/DQ_GoButton';
+import DQ_Badge from '../../components/DQ_Badge';
+
+const imageMapping: {[key: string]: any} = {
+  'health.png': require('../../assets/images/health.png'),
+  'life.png': require('../../assets/images/life.png'),
+  'motor.png': require('../../assets/images/motor.png'),
+  'property.png': require('../../assets/images/property.png'),
+  'personal.png': require('../../assets/images/personal.png'),
+  'travel.png': require('../../assets/images/travel.png'),
+  'investment.png': require('../../assets/images/investment.png'),
+  'expat.png': require('../../assets/images/expat.png'),
+  'liability.png': require('../../assets/images/liability.png'),
+  'marine.png': require('../../assets/images/marine.png'),
+  'engrisk.png': require('../../assets/images/engrisk.png'),
+  'other.png': require('../../assets/images/other.png'),
+  'protection.png': require('../../assets/images/protection.png'),
+};
+
+const Item = ({name, groupCode, nbrPolicies}: {name: string; groupCode: string, nbrPolicies:number}) => {
+  const imageName = `${groupCode.toLowerCase()}.png`;
+
+  return (
+    <View style={styles.Image_Container}>
+      <View style={styles.Inline_Image}>
+        <Image
+          source={imageMapping[imageName]}
+          style={styles.Rounded_Image}
+          resizeMode="contain"
+        />
+      </View>
+      {nbrPolicies > 0 && <DQ_Badge text={nbrPolicies}/>}
+      <View style={styles.InlineText}>
+        <Text style={styles.Product_Name}>{name}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default function ProductPolicy({navigation}: any) {
+  const [prodGroups, setProdGroups] = useState<any[]>([]);
+  const [osPremiums, setOsPremiums] = useState<any[]>([]);
+  const [osClaims, setOsClaims] = useState<any[]>([]);
+  const [pendingRequests, setPendingRequests] = useState<any[]>([]);
+  const [pendingRenewals, setPendingRenewals] = useState<any[]>([]);
+
+  useEffect(() => {
+    Get_CS_Connect();
+  }, []);
+
+  const Get_CS_Connect = async () => {
+    const result = await ProductPolicyService()
+    
+    if (result ) {
+      if (result.prodGroups) {
+        const sortedProdGroups = result.prodGroups.sort((a : any, b : any) => a.groupSeq - b.groupSeq);
+        setProdGroups(sortedProdGroups);
+      }
+      if(result.osPremiums) setOsPremiums(result.osPremiums);
+      if(result.osClaims) setOsClaims(result.osClaims);
+      if(result.pendingRequests) setPendingRequests(result.pendingRequests);
+      if(result.pendingRenewals) setPendingRenewals(result.pendingRenewals);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <DQ_BaseHeader
+        style={styles.mainHeader}
+        press={() => navigation.goBack()}
+        navigation={navigation}
+      />
+      <ScrollView>
+        <View style={styles.Products_Container}>
+          <FlatList
+            horizontal
+            data={prodGroups}
+            renderItem={({item}) => (
+              <Item name={item.groupName} groupCode={item.groupCode} nbrPolicies={item.nbrPolicies} />
+            )}
+            keyExtractor={item => item.groupSeq.toString()}
+          />
+        </View>
+        <View style={styles.cardsContainer}>
+          {osPremiums && (
+            <DQ_Card
+              title="My Outstanding Premiums"
+              count={osPremiums[0]?.nbrPremiums}>
+              <DQ_InnerCard_Grid buttonText="Pay Online" buttonWidth={120}>
+                <View style={styles.InlineElements}>
+                  <DQ_Paragraph
+                    content={osPremiums[0]?.nbrPremiums}
+                    fontSize={14}
+                  />
+                  <DQ_Paragraph content={'Premiums'} fontSize={14} />
+                  <DQ_Paragraph
+                    content={osPremiums[0]?.fresh ? 'Fresh' : ''}
+                    fontSize={14}
+                  />
+                  <DQ_Paragraph
+                    content={
+                      osPremiums[0]?.osAmount + ' ' + osPremiums[0]?.currency
+                    }
+                    textColor="#7dadd6"
+                    fontSize={14}
+                  />
+                </View>
+              </DQ_InnerCard_Grid>
+            </DQ_Card>
+          )}
+          {osClaims && (
+            <DQ_Card title="My Claims" count={osClaims[0]?.nbrOSClaims}>
+              <DQ_InnerCard_Grid buttonText="Check My Claims" buttonWidth={160}>
+                <View style={styles.TwoInlineElements}>
+                  <DQ_Paragraph
+                    content={osClaims[0]?.nbrOSClaims}
+                    fontSize={14}
+                  />
+                  <DQ_Paragraph content={'Outstanding Claims'} fontSize={14} />
+                  <DQ_Paragraph content={''} fontSize={14} />
+                  <DQ_Paragraph content={''} fontSize={14} />
+                </View>
+                <View style={styles.InlineElements}>
+                  <DQ_Paragraph
+                    content={osClaims[0]?.nbrReadyToSettle}
+                    fontSize={14}
+                  />
+                  <View style={{width: 70}}>
+                    <DQ_Paragraph content={'Ready to Settle'} fontSize={14} />
+                  </View>
+                  <DQ_Paragraph
+                    content={osClaims[0]?.fresh ? 'Fresh' : ''}
+                    fontSize={14}
+                  />
+                  <DQ_Paragraph
+                    content={
+                      osClaims[0]?.r2SAmount + ' ' + osClaims[0]?.currency
+                    }
+                    textColor="#7dadd6"
+                    fontSize={14}
+                  />
+                </View>
+              </DQ_InnerCard_Grid>
+            </DQ_Card>
+          )}
+          {pendingRenewals && (
+            <DQ_GoButton
+              title="Renewals"
+              count={pendingRenewals[0]?.nbrRenewals}
+            />
+          )}
+          {pendingRequests && (
+            <DQ_GoButton
+              title="Pending Requests"
+              count={pendingRequests[0]?.nbrRequests}
+            />
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  Products_Container: {
+    paddingTop: 15,
+  },
+  Image_Container: {
+    padding: 1,
+    marginTop:5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 110,
+    flex: 1,
+  },
+  Product_Name: {
+    marginTop: 10,
+    textAlign: 'center',
+    width: 80,
+    flexWrap: 'wrap',
+    color: '#005faf',
+    fontWeight: '700',
+  },
+  Rounded_Image: {
+    width: 45,
+    height: 45,
+    padding: 10,
+  },
+  Inline_Image: {
+    borderWidth: 1,
+    borderColor: '#175384',
+    borderRadius: 60,
+    padding: 7,
+    flex: 0.5,
+  },
+  InlineText: {
+    flex: 0.5,
+  },
+  mainHeader: {
+    flex: 1,
+    borderColor: 'red',
+  },
+  cardsContainer:{
+    marginTop:30,
+  },
+  InlineElements:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:5,
+  },
+  TwoInlineElements:{
+    flexDirection:'row',
+    gap:30,
+    alignItems:'center',
+    padding:5,
+  }
+});
